424491a4331a34ce17ca6dd3223b91664968811d
1100024535 <1100024535@L815WD220034.edu.cbe.ab.ca>
2023-09-12 09:43:34 -0600

    9:43 AM

---
+++ TEST1/src/Test1.java
@@ -0,0 +1,17 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		//Declaration and Initialize
+		int age = 16;
+		double cost = 92.45;
+		String name = "messi";
+		char letter = 'A';
+		
+		System.out.print(name + "=" +(age * cost) );
+		//System.out.print("WONDERFULL\n");
+		
+	}
+
+}
