8471bad4292608c7eea0b8e39bdc7ab61fa94175
1100024535 <1100024535@L815WD220034.edu.cbe.ab.ca>
2023-09-13 11:07:18 -0600

    11:07

--- TEST1/src/Test1.java
+++ TEST1/src/Test1.java
@@ -1,17 +1,30 @@
+import java.util.Scanner;
 
 public class Test1 {
 
 	public static void main(String[] args) {
 		
 	
-		int age = 16;
-		double cost = 92.45;
-		String name = "messi";
-		char letter = 'A';
+		Scanner input = new Scanner(System.in);
 		
-		System.out.print(name + "=" +(age * cost) );
+		System.out.print("Eneter the first number: ");
+		
+		double num1 = input.nextDouble();
+		
+		System.out.print("Eneter the second number: ");
 		//System.out.print("WONDERFULL\n");
+		
+		
+		
+		
+		double num2 = input.nextDouble();
+		double sum = num1 + num2;
+		
+		System.out.print("the sum of the numbers are: " + sum );
+		
+		
 		
 	}
 
 }
+//IPO
